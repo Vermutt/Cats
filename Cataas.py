3d0def56939bee4bf08df1e4f10e9ba035680e5a
Vermutt <vera.python41@gmail.com>
2024-08-06 21:20:09 +0300

    Добавлено получение изображений по тегу, а также без тега

--- Cataas.py
+++ Cataas.py
@@ -18,7 +18,9 @@ def load_image(url):
 
 
 def open_new_window():
-    img = load_image(url)
+    tag = tag_entry.get()
+    url_tag = f"https://rand.by/image/{tag}" if tag else "https://rand.by/image"
+    img = load_image(url_tag)
 
     if img:
         new_window = Toplevel()
@@ -38,10 +40,11 @@ window = Tk()
 window.title("Cats!")
 window.geometry("600x520")
 
+tag_entry = Entry()
+tag_entry.pack()
 
-
-# update_button = Button(text="Обновить!", command=set_image)
-# update_button.pack()
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
 
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
@@ -52,7 +55,5 @@ file_menu.add_command(label="Загрузить фото", command=open_new_wind
 file_menu.add_command(label="Выход", command=exit)
 
 url = "https://rand.by/image"
-
-set_image()
 
 window.mainloop()
